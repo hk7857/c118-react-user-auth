0a78fd020b8e5f5a8e1c7eb9e85a7185bb7ebf9c
Hanran <hanrankim@gmail.com>
2018-04-11 14:54:31 -0700

    completed setup redux

---
+++ src/reducers/user_reducer.js
@@ -0,0 +1,12 @@
+import types from '../action/types';
+
+const DEFAULT_STATE = {
+    auth: false
+};
+
+export default (state = DEFAULT_STATE, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
